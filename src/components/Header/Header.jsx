c7acd768bbfc7fc5d98a4ba910685ff3a8e778ea
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-04-18 21:07:14 -0400

    NavLink for active route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,10 +9,20 @@ const Header = () => {
 			<img className="header__logo" src={logoImg} alt="bandsite" />
 			<nav className="nav">
 				<ul className="nav__list">
-					<NavLink className="nav__link nav__link--active" to="/biography">
+					<NavLink
+						className={({ isActive }) =>
+							isActive ? "nav__link nav__link--active" : "nav__link"
+						}
+						to="/biography"
+					>
 						<li className="nav__item">Biography</li>
 					</NavLink>
-					<NavLink className="nav__link" to="/shows">
+					<NavLink
+						className={({ isActive }) =>
+							isActive ? "nav__link nav__link--active" : "nav__link"
+						}
+						to="/shows"
+					>
 						<li className="nav__item">Shows</li>
 					</NavLink>
 				</ul>
