9c929ebf9ac518a7571fe544cbaa45dc95d959f3
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-05-03 11:49:47 -0400

    ticket details styles

--- src/components/TicketDetails/TicketDetails.jsx
+++ src/components/TicketDetails/TicketDetails.jsx
@@ -40,29 +40,43 @@ const TicketDetails = () => {
 		<section className="tiketDetails">
 			<h2 className="tiketDetails__heading">Event Details</h2>
 			<main className="tiketDetails__main-container">
-				<div className="tiketDetails__details">
-					<p>Date: {new Date(Number(ticket.date)).toDateString()}</p>
-					<p>Venue: {ticket.place}</p>
-					<p>Location: {ticket.location}</p>
-					<p>Price: $50</p>
+				<div className="tiketDetails__details--wrapper">
+					<div className="tiketDetails__details">
+						<p>
+							<span>Date: </span>
+							{new Date(Number(ticket.date)).toDateString()}
+						</p>
+						<p>
+							<span>Venue: </span>
+							{ticket.place}
+						</p>
+						<p>
+							<span>Location: </span>
+							{ticket.location}
+						</p>
+						<p>
+							<span>Price: </span>$50
+						</p>
+					</div>
+					<div className="tiketDetails__details">
+						<h2>About the show</h2>
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+							animi magnam officiis, sit, perferendis accusantium quo impedit
+							distinctio quas mollitia ut tenetur asperiores eaque rerum debitis
+							amet facilis quos labore veritatis neque, dignissimos doloribus.
+							Harum modi illum minus aperiam excepturi nam sit nisi minima ipsum
+							ut commodi, unde consequatur praesentium earum vel repudiandae
+							magni possimus hic inventore, culpa laborum? Laudantium illum
+							tempora laborum inventore, provident et voluptates! Vel sunt,
+							suscipit molestias tempore nesciunt consequuntur ratione assumenda
+							sint quibusdam soluta ullam aspernatur tempora laborum eaque
+							repudiandae ipsam, excepturi dolorum sit atque! Molestias
+							provident dolorum fuga ipsum possimus aut commodi assumenda hic..
+						</p>
+					</div>
 				</div>
-				<div className="tiketDetails__details">
-					<p>
-						About the show: Lorem ipsum dolor, sit amet consectetur adipisicing
-						elit. Quod animi magnam officiis, sit, perferendis accusantium quo
-						impedit distinctio quas mollitia ut tenetur asperiores eaque rerum
-						debitis amet facilis quos labore veritatis neque, dignissimos
-						doloribus. Harum modi illum minus aperiam excepturi nam sit nisi
-						minima ipsum ut commodi, unde consequatur praesentium earum vel
-						repudiandae magni possimus hic inventore, culpa laborum? Laudantium
-						illum tempora laborum inventore, provident et voluptates! Vel sunt,
-						suscipit molestias tempore nesciunt consequuntur ratione assumenda
-						sint quibusdam soluta ullam aspernatur tempora laborum eaque
-						repudiandae ipsam, excepturi dolorum sit atque! Molestias provident
-						dolorum fuga ipsum possimus aut commodi assumenda hic..
-					</p>
-				</div>
-				<div className="actions">
+				<div className="tiketDetails__actions">
 					<Button onClick={() => navigate(-1)}>Go Back</Button>
 					<Link to={`purchase`}>
 						<Button>Buy Ticket</Button>
